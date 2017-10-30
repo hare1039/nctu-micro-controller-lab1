1b8d0fad9a6e6bbc6d5690492e84dcd3295853a1
hare1039 <hare39393@gmail.com>
2017-10-30 23:14:45 +0800

    finish-delay

--- src/main.s
+++ src/main.s
@@ -3,136 +3,110 @@
 	.thumb
 
 .data
-	user_stack:  .zero 128
-	expr_result: .word   0
-	stoi_error:  .word   0
+	arr: .byte 0b01111110, 0b00110000, 0b01101101, 0b01111001, 0b00110011, 0b01011011, 0b01011111, 0b01110000, 0b01111111, 0b01111011, 0b01110111, 0b00011111, 0b01001110, 0b00111101, 0b01001111, 0b01000111
+	ONE_SEC: .word 800000
+	@ arr: a0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, b, C, d, E, F
 
 .text
 	.global main
-	postfix_expr: .asciz "70 0 -1 + 50 + -"
-	.align 4
 
-stoi: // func stoi(char *:r1, int& start:r2) -> int:r0
-	PUSH {r3-r7}
-	mov  r4, #1         // int positive = 1;
-	ldrb r5, [r1, r2]   // char r5 = r1[pos]
-    cmp  r5, #45        // if (r1[pos] == '-')
-    beq IF_0000_TRUE
-    b IF_0000_ELSE
-	IF_0000_TRUE:
-   	    add r2, r2, #1     // pos = pos + 1;
-    	    mov r4, #-1        // positive = -1;
-    	    b END_IF_0000
-    IF_0000_ELSE:
-	    cmp r5, #43        // else if(r1[pos] == '+')
-        beq IF_0001_TRUE
-        b   IF_0001_ELSE
-        IF_0001_TRUE:
-	        add r2, r2, #1 // pos = pos + 1;
-	        b END_IF_0001
-        IF_0001_ELSE:
-        END_IF_0001:
-    END_IF_0000:
+	.equ RCC_AHB2ENR,  0x4002104C
 
+	.equ DECODE_MODE,  0x09
+	.equ DISPLAY_TEST, 0x0F
+	.equ SCAN_LIMIT,   0x0B
+	.equ INTENSITY,    0x0A
+	.equ SHUTDOWN,     0x0C
 
-	mov r0, #0               // num = 0;
-	mov r3, #1               // error = 1
-    WHILE_0000:              // while(true)
-	    ldrb r5, [r1, r2]    // char x = r1[pos]
-    		cmp  r5, #32         // if (x == ' ')
-        beq  IF_0003_TRUE
-        b    IF_0003_ELSE
-        IF_0003_TRUE:
-        		add r2, r2, #1
-            b END_WHILE_0000 // break;
-        IF_0003_ELSE:
-        END_IF_0003:
+	.equ MAX7219_DIN,  0x20 @ PA5
+	.equ MAX7219_CS,   0x40 @ PA6
+	.equ MAX7219_CLK,  0x80 @ PA7
 
-        cmp r5, #0        // if (x == '\0')
-        beq IF_0004_TRUE
-        b   IF_0004_ELSE
-        IF_0004_TRUE:
-            b END_WHILE_0000 // break;
-        IF_0004_ELSE:
-        END_IF_0004:
+	.equ GPIOA_BASE,   0x48000000
+	.equ BSRR_OFFSET,  0x18 @ set bit
+	.equ BRR_OFFSET,   0x28 @ clear bit
+	.equ ONE_SEC,      4000000
 
-		mov r3, #0           // error = false
-        add r2, r2, #1       // pos = pos + 1
-        mov r6, #10          // load r6 as 10
-        sub r5, r5, #48      // x = x - '0'
-        mla r0, r0, r6, r5   // num = num * 10 + x
-        b WHILE_0000
-    END_WHILE_0000:
+gpio_init:
+	mov  r0, 0b00000000000000000000000000000001
+	ldr  r1, =RCC_AHB2ENR
+	str  r0, [r1]
 
-    mul r0, r0, r4
-    cmp r0, #0
-    beq IF_0002_TRUE
-    b   IF_0002_ELSE
-    IF_0002_TRUE:
-        cmp r3, #1
-    		beq IF_0007_TRUE
-   		b   IF_0007_ELSE
-   		IF_0007_TRUE:
-   			mov r0, #254
-   			ldr r6, =stoi_error
-    	    		str r4, [r6]
-   			b END_IF_0007
-   		IF_0007_ELSE:
-   		END_IF_0007:
-    	    b END_IF_0002
-    IF_0002_ELSE:
-    END_IF_0002:
- 	POP {r3-r7}
-    BX LR
-// end stoi
+	ldr  r1, =GPIOA_BASE @ GPIOA_MODER
+	ldr  r2, [r1]
+	and  r2, 0b11111111111111110000001111111111
+	orr  r2, 0b00000000000000000101010000000000
+	str  r2, [r1]
+
+	add  r1, 0x4 @ GPIOA_OTYPER
+	ldr  r2, [r1]
+	and  r2, 0b11111111111111111111111100011111
+	str  r2, [r1]
+
+	add  r1, 0x4 @ GPIOA_SPEEDER
+	ldr  r2, [r1]
+	and  r2, 0b11111111111111110000001111111111
+	orr  r2, 0b00000000000000000101010000000000
+	str  r2, [r1]
+
+	bx   lr
+
+max7219_init:
+	push {r0, r1, r2, lr}
+
+	ldr  r0, =DECODE_MODE
+	ldr  r1, =0x0
+	bl   max7219_send
+
+	ldr  r0, =DISPLAY_TEST
+	ldr  r1, =0x0
+	bl   max7219_send
+
+	ldr  r0, =SCAN_LIMIT
+	ldr  r1, =0x0
+	bl   max7219_send
+
+	ldr  r0, =INTENSITY
+	ldr  r1, =0xA
+	bl   max7219_send
+
+	ldr  r0, =SHUTDOWN
+	ldr  r1, =0x1
+	bl   max7219_send
+
+	pop  {r0, r1, r2, pc}
+
 
 
 main:
-	ldr	r1, =postfix_expr
-	ldr SP, =user_stack
-	mov r2, #0
-	add SP, SP, 128
+	bl   gpio_init
+	bl   max7219_init
 
-	// setup argument done
-	bl stoi
-	PUSH {r0}
-	bl stoi
-	PUSH {r0}
-	WHILE_LOOP_0001:
-		ldr r3, =user_stack
-		add r3, r3, #128
-		cmp SP, r3
-		beq IF_0005_TRUE
-		b IF_0005_ELSE
-		IF_0005_TRUE:
-			b END_WHILE_LOOP_0001
-		IF_0005_ELSE:
-		END_IF_0005:
 
-		bl stoi
+	Display0toFLoop:
 
-		cmp r0, #254 // 254 == failed
-		beq IF_0006_TRUE
-		b   IF_0006_ELSE
-		IF_0006_TRUE:
-			ldr r4, =stoi_error
-			ldr r4, [r4]
-			POP {r5, r6}
-			mul r5, r5, r4
-			add r5, r6, r5
-			PUSH {r5}
-			b END_IF_0006
-		IF_0006_ELSE:
-			PUSH {r0}
-		END_IF_0006:
-		b WHILE_LOOP_0001
-	END_WHILE_LOOP_0001:
 
-	POP {r5}
-	ldr r4, =expr_result
-	str r5, [r4]
 
-    b program_end
-program_end:
-	B		program_end
+	b Display0toFLoop
+
+MAX7219Send:
+	push {r0, r1, r2, r3, r4, r5, r6, r7, r8, lr}
+	BX LR
+
+MAX7219_init:
+	//TODO: Initialize max7219 registers
+	BX LR
+
+delay:
+	push {r0}
+	ldr r0, =ONE_SEC
+	ldr r0, [r0]
+	WHILE_LOOP_DELAY:
+		cmp r0, #0
+		beq END_WHILE_LOOP_DELAY
+		sub r0, r0, #1
+		b WHILE_LOOP_DELAY
+	END_WHILE_LOOP_DELAY:
+	pop {r0}
+	BX LR
 
